45561a7bec5e348bbec7e27ed4a59943af0de94f
Isaiah Simon <60068759+IsaiahSimon@users.noreply.github.com>
2022-11-25 13:39:33 -0500

    create Navbar & add styles

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,76 @@
-import React from 'react';
+import React from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdOutlineRestaurantMenu } from "react-icons/md";
 
-import './Navbar.css';
+import images from "../../constants/images";
+import "./Navbar.css";
 
 const Navbar = () => (
-  <div>
-    Navbar
-  </div>
+  <nav className="app__navbar">
+    {/* Logo */}
+    <div className="app__navbar-logo">
+      <img src={images.chateauChai} alt="logo" />
+    </div>
+
+    {/* Main navigation links */}
+    <ul className="app__navbar-links">
+      <li className="p__opensans">
+        <a href="#home">Home</a>
+      </li>
+      <li className="p__opensans">
+        <a href="#about">About</a>
+      </li>
+      <li className="p__opensans">
+        <a href="#menu">Menu</a>
+      </li>
+      <li className="p__opensans">
+        <a href="#awards">Awards</a>
+      </li>
+      <li className="p__opensans">
+        <a href="#contact">Contact</a>
+      </li>
+    </ul>
+
+    {/* Log In/Register and Book Table links */}
+    <div className="app__navbar-login">
+      <a href="#login" className="p__opensans">
+        Log In / Register
+      </a>
+      <div />
+      <a href="/" className="p__opensans">
+        Book Table
+      </a>
+    </div>
+
+    {/* Hamburger Menu on Mobile */}
+    <div className="app__navbar-smallscreen">
+      <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => {}} />
+      <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+        <MdOutlineRestaurantMenu
+          fontSize={27}
+          className="overlay__close"
+          onClick={() => {}}
+        />
+        <ul className="app__navbar-smallscreen-links">
+          <li className="p__opensans">
+            <a href="#home">Home</a>
+          </li>
+          <li className="p__opensans">
+            <a href="#about">About</a>
+          </li>
+          <li className="p__opensans">
+            <a href="#menu">Menu</a>
+          </li>
+          <li className="p__opensans">
+            <a href="#awards">Awards</a>
+          </li>
+          <li className="p__opensans">
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 );
 
 export default Navbar;
