75619c4cc82d68540502a8922beee32a867a9c6b
Isaiah Simon <60068759+IsaiahSimon@users.noreply.github.com>
2022-11-25 17:45:54 -0500

    create SpecialMenu, MenuItem & add styles

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React from "react";
 
-import './MenuItem.css';
+import "./MenuItem.css";
 
-const MenuItem = () => (
-  <div>
-    MenuItem
+const MenuItem = ({ title, price, tags }) => (
+  <div className="app__menuItem">
+    <div className="app__menuItem-head">
+      {/* Title */}
+      <div className="app__menuItem-name">
+        <p className="p__cormorant p__color">{title}</p>
+      </div>
+
+      <div className="app__menuItem-dash" />
+
+      {/* Price */}
+      <div className="app__menuItem-price">
+        <p className="p__cormorant">{price}</p>
+      </div>
+    </div>
+
+    {/* Tags */}
+    <div className="app__menuItem-sub">
+      <p className="p__opensans">{tags}</p>
+    </div>
   </div>
 );
 
