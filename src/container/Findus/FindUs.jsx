101cb3030e66ee59381b631ecdb3d197d289f2dd
Isaiah Simon <60068759+IsaiahSimon@users.noreply.github.com>
2022-11-26 13:51:21 -0500

    create FindUs & add styles

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,8 +1,27 @@
-import React from 'react';
+import React from "react";
+
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
+import "./FindUs.css";
 
 const FindUs = () => (
-  <div>
-    FindUs
+  <div className="app__bg app__wrapper section__padding" id="contact">
+    <div className="app__wrapper_info">
+      <SubHeading title="Contact" />
+      <h1 className="headtext__cormorant">Find Us</h1>
+      <div className="app__wrapper-content">
+        <p className="p__opensans">123 Mulberry Ln, West Hanover, CT 12031</p>
+        <p className="p__cormorant">Opening Hours</p>
+        <p className="p__opensans">Mon - Fri: 10:00 am - 11:00 pm</p>
+        <p className="p__opensans">Sat - Sun: 10:00 am - 12:00 am</p>
+      </div>
+      <button type="button" className="custom__button">
+        Visit Us
+      </button>
+    </div>
+    <div className="app__wrapper_img">
+      <img src={images.findus} alt="findus" />
+    </div>
   </div>
 );
 
