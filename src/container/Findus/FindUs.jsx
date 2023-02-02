6503b14a1ee11ab4078967d23636e260b41d8ef6
Isaiah Simon <60068759+IsaiahSimon@users.noreply.github.com>
2023-02-01 20:43:30 -0500

    update info

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -5,22 +5,22 @@ import { images } from "../../constants";
 import "./FindUs.css";
 
 const FindUs = () => (
-  <div className="app__bg app__wrapper section__padding" id="contact">
-    <div className="app__wrapper_info">
-      <SubHeading title="Contact" />
-      <h1 className="headtext__cormorant">Find Us</h1>
-      <div className="app__wrapper-content">
-        <p className="p__opensans">59 Culinary Dr, Rehoboth Beach, DE 19971</p>
-        <p className="p__cormorant">Opening Hours</p>
-        <p className="p__opensans">Mon - Fri: 11:00 am - 11:00 pm</p>
-        <p className="p__opensans">Sat - Sun: 11:00 am - 01:00 am</p>
+  <div className='app__bg app__wrapper section__padding' id='contact'>
+    <div className='app__wrapper_info'>
+      <SubHeading title='Contact' />
+      <h1 className='headtext__cormorant'>Find Us</h1>
+      <div className='app__wrapper-content'>
+        <p className='p__opensans'>Rehoboth Beach, DE 19971</p>
+        <p className='p__cormorant'>Opening Hours</p>
+        <p className='p__opensans'>Mon - Fri: 11:00 am - 11:00 pm</p>
+        <p className='p__opensans'>Sat - Sun: 11:00 am - 01:00 am</p>
       </div>
-      <button type="button" className="custom__button">
+      <button type='button' className='custom__button'>
         Visit Us
       </button>
     </div>
-    <div className="app__wrapper_img">
-      <img src={images.findus} alt="findus" />
+    <div className='app__wrapper_img'>
+      <img src={images.findus} alt='findus' />
     </div>
   </div>
 );
--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -8,44 +8,44 @@ import "./Footer.css";
 const today = new Date();
 
 const Footer = () => (
-  <div className="app__footer section__padding">
+  <div className='app__footer section__padding'>
     <FooterOverlay />
     <Newsletter />
 
-    <div className="app__footer-links">
-      <div className="app__footer-links_contact">
-        <h1 className="app__footer-headtext">Contact Us</h1>
-        <p className="p__opensans">59 Culinary Dr, Rehoboth Beach, DE 19971</p>
-        <p className="p__opensans">+1 302-567-8901</p>
-        <p className="p__opensans">+1 302-567-8907</p>
+    <div className='app__footer-links'>
+      <div className='app__footer-links_contact'>
+        <h1 className='app__footer-headtext'>Contact Us</h1>
+        <p className='p__opensans'>Rehoboth Beach, DE 19971</p>
+        <p className='p__opensans'>+1 302-123-4567</p>
+        <p className='p__opensans'>+1 302-123-4567</p>
       </div>
-      <div className="app__footer-links_logo">
-        <img src={images.chateauChai} alt="footer_logo" />
-        <p className="p__opensans">
+      <div className='app__footer-links_logo'>
+        <img src={images.chateauChai} alt='footer_logo' />
+        <p className='p__opensans'>
           "The best way to find yourself is to lose yourself in the service of
           others." – Mahatma Gandhi
         </p>
-        <img src={images.spoon} alt="spoon" className="spoon__img" />
-        <div className="app__footer-links_icons">
+        <img src={images.spoon} alt='spoon' className='spoon__img' />
+        <div className='app__footer-links_icons'>
           <FiFacebook />
           <FiTwitter />
           <FiInstagram />
         </div>
       </div>
-      <div className="app__footer-links_work">
-        <h1 className="app__footer-headtext">Working Hours</h1>
-        <p className="p__opensans">Monday - Friday:</p>
-        <p className="p__opensans">11:00 am - 11:00 pm</p>
-        <p className="p__opensans">Saturday - Sunday:</p>
-        <p className="p__opensans">11:00 am - 01:00 am</p>
+      <div className='app__footer-links_work'>
+        <h1 className='app__footer-headtext'>Working Hours</h1>
+        <p className='p__opensans'>Monday - Friday:</p>
+        <p className='p__opensans'>11:00 am - 11:00 pm</p>
+        <p className='p__opensans'>Saturday - Sunday:</p>
+        <p className='p__opensans'>11:00 am - 01:00 am</p>
       </div>
     </div>
 
-    <div className="footer__copyright">
-      <p className="p__opensans">
+    <div className='footer__copyright'>
+      <p className='p__opensans'>
         Copyright © {today.getFullYear()} <span>Chateau Chai</span>. All Rights
         reserved. Crafted by
-        <a href="https://isimon.dev/" target="_blank" rel="noreferrer">
+        <a href='https://isimon.dev/' target='_blank' rel='noreferrer'>
           {" "}
           Isaiah Simon | iSimonDev
         </a>
